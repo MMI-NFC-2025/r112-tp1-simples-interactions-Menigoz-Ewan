105c31ab07d573ee7fdc3fe0bd6eb2ad30cdadc5
Menigoz-Ewan <ewan.menigoz@edu.univ-fcomte.fr>
2025-11-07 11:52:01 +0100

    fin

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 // TODO
+const aMettreEnrouge = document.querySelector('#a-mettre-en-rouge')
+aMettreEnrouge.style.color = "red";
+
+const enRougeSuiteAclick = document.querySelector('#en-rouge-suite-a-click')
+enRougeSuiteAclick.addEventListener("click", (evt) => {
+    enRougeSuiteAclick.style.color = 'red'
+});
